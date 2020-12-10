9ff6ff31fc6e78a24cd758d487ca7aba211a899e
richard jones {richard.t.jones@uconn.edu} <rtj02001@cn410.storrs.hpc.uconn.edu>
2020-12-10 09:37:37 -0500

    * new osgprod utility to merge the results from OSG raw data   production at the level of raw data files, and save the merged   files in the /recon/verXX tree to transmission back to Jlab. [rtj]

---
+++ osgprod/osgprod_bind.py
@@ -0,0 +1,305 @@
+#!/usr/bin/env python3
+#
+# osgprod_bind.py - tool to find the scattered output files
+#                   from Gluex production of raw data on the osg,
+#                   and merge the outputs into the standard format
+#                   used for archival on tape storage.
+#
+# author: richard.t.jones at uconn.edu
+# version: december 8, 2020
+
+import os
+import re
+import struct
+import psycopg2
+import subprocess
+import shutil
+
+dbserver = "cn445.storrs.hpc.uconn.edu"
+dbname = "osgprod"
+dbuser = "gluex"
+dbpass = "slicing+dicing"
+
+input_area = "/dcache/gluex/resilient"
+output_area = "/dcache/gluex/resilient/recon/ver01"
+
+def db_connection():
+   """
+   Returns a persistent connection to the osgprod database.
+   """
+   global dbconnection
+   try:
+      conn = dbconnection
+   except:
+      dbconnection = psycopg2.connect(user = dbuser,
+                                      password = dbpass,
+                                      host = dbserver,
+                                      port = "5432",
+                                      database = dbname)
+      conn = dbconnection
+   return conn
+
+def create_table_bindings(delete=False):
+   with db_connection().cursor() as cursor:
+      if delete:
+         cursor.execute("DROP TABLE bindings;")
+      cursor.execute("""CREATE TABLE bindings
+                        (id          SERIAL  PRIMARY KEY     NOT NULL,
+                         iraw        INT     REFERENCES rawdata(id),
+                         starttime   TIMESTAMP WITH TIME ZONE,
+                         endtime     TIMESTAMP WITH TIME ZONE,
+                         exitcode    INT,
+                         UNIQUE(iraw));
+                     """)
+   dbconnection.commit()
+
+def init_output_area(area):
+   """
+   Creates the directory structure in output_area to receive the
+   merged results from this module.
+   """
+   for i in range(2, len(area)):
+      subdir = "/".join(area.split("/")[:i])
+      if not os.path.isdir(subdir):
+         os.mkdir(subdir)
+
+def next():
+   """
+   Gets the next output set to bind from the database, unpacks them into
+   a temporary directory, and merges them at the input data file level
+   into files in the output directory tree under the top-level dir vers.
+   If the bindings db does not already exit, you must manually call
+   create_table_bindings before trying to invoke this function.
+   Return value is 1 if one rawdata file was processed successfully, 0
+   if no data were found ready for binding, or < 0 if an error occurred.
+   """
+   iraw = 0
+   run = 0
+   seqno = 0
+   with db_connection().cursor() as cursor:
+      cursor.execute("""SELECT rawdata.id,rawdata.run,rawdata.seqno,
+                               rawdata.nblocks,slices.block1,slices.block2,
+                               jobs.cluster,jobs.process
+                        FROM rawdata
+                        LEFT JOIN bindings
+                        ON bindings.iraw = rawdata.id
+                        INNER JOIN slices
+                        ON slices.iraw = rawdata.id
+                        INNER JOIN jobs
+                        ON slices.ijob = jobs.id
+                        WHERE bindings.id IS NULL
+                        AND jobs.exitcode = 0
+                        ORDER BY rawdata.id,slices.block1;
+                     """)
+      blocks2do = {}
+      for row in cursor.fetchall():
+         i = int(row[0])
+         if i != iraw:
+            if len(blocks2do) == 1:
+               break
+            else:
+               run = int(row[1])
+               seqno = int(row[2])
+               nblocks = int(row[3])
+               blocks2do = set(range(0,nblocks))
+               slices = []
+               iraw = i
+         block1 = int(row[4])
+         block2 = int(row[5])
+         blocks2do ^= set(range(block1,block2))
+         cluster = int(row[6])
+         process = int(row[7])
+         slices.append((block1,block2,cluster,process))
+      if len(blocks2do) != 1:
+         return 0
+      else:
+         cursor.execute("SELECT TIMEZONE('GMT', NOW());")
+         now = cursor.fetchone()[0]
+         cursor.execute("""INSERT INTO bindings
+                           (iraw,starttime)
+                           VALUES (%s,%s)
+                           RETURNING id;
+                        """, (iraw, now))
+         row = cursor.fetchone()
+         if row:
+            ibind = int(row[0])
+         else:
+            return 0
+      db_connection().commit()
+
+   workdir = str(iraw)
+   os.mkdir(workdir)
+   os.chdir(workdir)
+   for sl in slices:
+      sdir = str(sl[0]) + "," + str(sl[1])
+      os.mkdir(sdir)
+      untar = ["tar", "zxf", input_area + "/" +
+               "job_{0}_{1}.tar.gz".format(sl[2], sl[3]),
+               "-C", sdir]
+      if subprocess.Popen(untar).wait() != 0:
+         return 0
+   exitcode = 0
+   exitcode += merge_evio_skims(run, seqno, slices)
+   exitcode += merge_hddm_output(run, seqno, slices)
+   exitcode += merge_job_info(run, seqno, slices)
+   exitcode += merge_root_histos(run, seqno, slices)
+   os.chdir("..")
+   shutil.rmtree(workdir)
+
+   with db_connection().cursor() as cursor:
+      cursor.execute("SELECT TIMEZONE('GMT', NOW());")
+      now = cursor.fetchone()[0]
+      cursor.execute("""UPDATE bindings
+                        SET endtime=%s, exitcode=%s
+                        WHERE id = %s;
+                     """, (now, exitcode, ibind))
+      db_connection().commit()
+   return 1
+
+def merge_evio_skims(run, seqno, slices):
+   """
+   Merge the special events skim files from the individual jobs
+   into a single skim file for each input raw data file. Returns
+   0 on success, nonzero on error.
+   """
+   inset = {"BCAL-LED": "hd_rawdata_{0:06d}_{1:03d}+{2},{3}.BCAL-LED.evio",
+            "DIRC-LED": "hd_rawdata_{0:06d}_{1:03d}+{2},{3}.DIRC-LED.evio",
+            "FCAL-LED": "hd_rawdata_{0:06d}_{1:03d}+{2},{3}.FCAL-LED.evio",
+            "CCAL-LED": "hd_rawdata_{0:06d}_{1:03d}+{2},{3}.CCAL-LED.evio",
+            "random": "hd_rawdata_{0:06d}_{1:03d}+{2},{3}.random.evio",
+            "omega": "hd_rawdata_{0:06d}_{1:03d}+{2},{3}.omega.evio",
+            "sync": "hd_rawdata_{0:06d}_{1:03d}+{2},{3}.sync.evio",
+            "ps": "hd_rawdata_{0:06d}_{1:03d}+{2},{3}.ps.evio",
+           }
+   outset = {"BCAL-LED": "BCAL-LED_{0:06d}_{1:03d}.evio",
+             "DIRC-LED": "DIRC-LED_{0:06d}_{1:03d}.evio",
+             "FCAL-LED": "FCAL-LED_{0:06d}_{1:03d}.evio",
+             "CCAL-LED": "CCAL-LED_{0:06d}_{1:03d}.evio",
+             "random": "random_{0:06d}_{1:03d}.evio",
+             "omega": "omega_{0:06d}_{1:03d}.evio",
+             "sync": "sync_{0:06d}_{1:03d}.evio",
+             "ps": "ps_{0:06d}_{1:03d}.evio",
+            }
+   for iset in inset:
+      ofile = outset[iset].format(run, seqno)
+      ifiles = ["{0},{1}/".format(sl[0], sl[1]) +
+                inset[iset].format(run, seqno, sl[0], sl[1])
+               for sl in slices
+               ]
+      cmd = ["eviocat", "-o", ofile] + ifiles
+      if subprocess.Popen(cmd).wait() != 0:
+         return -1
+      odir = output_area + "/" + iset + "/{0:06d}".format(run)
+      init_output_area(odir)
+      opath = odir + "/" + ofile
+      if os.path.exists(opath):
+         os.remove(opath)
+      shutil.copyfile(ofile, opath)
+   return 0
+
+def merge_root_histos(run, seqno, slices):
+   """
+   Merge the output root files from the individual jobs into
+   a single root file for each input raw data file. Returns
+   0 on success, nonzero on error.
+   """
+   inset = {"hists": "hd_root.root",
+            "tree_TS_scaler": "tree_TS_scaler.root",
+            "tree_bcal_hadronic_eff": "tree_bcal_hadronic_eff.root",
+            "tree_fcal_hadronic_eff": "tree_fcal_hadronic_eff.root",
+            "tree_tof_eff": "tree_tof_eff.root",
+            "tree_sc_eff": "tree_sc_eff.root",
+            "tree_PSFlux": "tree_PSFlux.root",
+            "tree_TPOL": "tree_TPOL.root",
+           }
+   outset = {"hists": "hd_root_{0:06d}_{1:03d}.root",
+             "tree_TS_scaler": "tree_TS_scaler_{0:06d}_{1:03d}.root",
+             "tree_bcal_hadronic_eff": "tree_bcal_hadronic_eff_{0:06d}_{1:03d}.root",
+             "tree_fcal_hadronic_eff": "tree_fcal_hadronic_eff_{0:06d}_{1:03d}.root",
+             "tree_tof_eff": "tree_tof_eff_{0:06d}_{1:03d}.root",
+             "tree_sc_eff": "tree_sc_eff_{0:06d}_{1:03d}.root",
+             "tree_PSFlux": "tree_PSFlux_{0:06d}_{1:03d}.root",
+             "tree_TPOL": "tree_TPOL_{0:06d}_{1:03d}.root",
+            }
+   for iset in inset:
+      ofile = outset[iset].format(run, seqno)
+      ifiles = ["{0},{1}/".format(sl[0], sl[1]) +
+                inset[iset].format(run, seqno, sl[0], sl[1])
+               for sl in slices
+               ]
+      cmd = ["hadd", ofile] + ifiles
+      if subprocess.Popen(cmd).wait() != 0:
+         return -1
+      odir = output_area + "/" + iset + "/{0:06d}".format(run)
+      init_output_area(odir)
+      opath = odir + "/" + ofile
+      if os.path.exists(opath):
+         os.remove(opath)
+      shutil.copyfile(ofile, opath)
+   return 0
+
+def merge_hddm_output(run, seqno, slices):
+   """
+   Merge the output hddm files from the individual jobs into
+   a single hddm file for each input raw data file. Returns
+   0 on success, nonzero on error.
+   """
+   inset = {"REST": "dana_rest.hddm",
+            "converted_random": "converted_random.hddm",
+           }
+   outset = {"REST": "dana_rest_{0:06d}_{1:03d}.hddm",
+             "converted_random": "converted_random_{0:06d}_{1:03d}.hddm",
+            }
+   for iset in inset:
+      ofile = outset[iset].format(run, seqno)
+      ifiles = []
+      for sl in slices:
+         ifile = "{0},{1}/".format(sl[0], sl[1]) +\
+                 inset[iset].format(run, seqno, sl[0], sl[1])
+         if iset == "converted_random" and not os.path.exists(ifile):
+            continue # missing converted_random is not an error
+         ifiles.append(ifile)
+      cmd = ["hddmcat", "-o", ofile] + ifiles
+      if subprocess.Popen(cmd).wait() != 0:
+         return -1
+      odir = output_area + "/" + iset + "/{0:06d}".format(run)
+      init_output_area(odir)
+      opath = odir + "/" + ofile
+      if os.path.exists(opath):
+         os.remove(opath)
+      shutil.copyfile(ofile, opath)
+   return 0
+
+def merge_job_info(run, seqno, slices):
+   """
+   Merge the job log hddm files from the individual jobs into
+   a catenated log file for each input raw data file. Returns
+   0 on success, nonzero on error.
+   """
+   inset = {"job_info": ["workscript.stdout", "workscript.stderr"],
+           }
+   outset = {"job_info": ["std_{0:06d}_{1:03d}.out", "std_{0:06d}_{1:03d}.err"],
+            }
+   tarset = {"job_info": "job_info_{0:06d}_{1:03d}.tgz",
+            }
+   for iset in inset:
+      outlist = []
+      for i in range(0, len(inset[iset])):
+         ofile = outset[iset][i].format(run, seqno)
+         with open(ofile, "w") as ostr:
+            for sl in slices:
+               ifile  = "{0},{1}/".format(sl[0], sl[1]) + inset[iset][i]
+               for lines in open(ifile):
+                  ostr.write(lines)
+         outlist.append(ofile)
+      tarfile = tarset[iset].format(run, seqno)
+      cmd = ["tar", "zcf", tarfile] + outlist
+      if subprocess.Popen(cmd).wait() != 0:
+         return -1
+      odir = output_area + "/" + iset + "/{0:06d}".format(run)
+      init_output_area(odir)
+      opath = odir + "/" + ofile
+      if os.path.exists(opath):
+         os.remove(opath)
+      shutil.copyfile(ofile, opath)
+   return 0
